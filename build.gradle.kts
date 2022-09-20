77f24b5137792d2375e327f09635899cb92fcb0b
Imran Malic Settuba <46971368+i-walker@users.noreply.github.com>
2022-09-20 20:36:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ repositories {
 
 dependencies {
   implementation(kotlin("stdlib"))
-  implementation("io.arrow-kt:arrow-core:1.1.3-rc.1")
-  implementation("io.arrow-kt:arrow-optics:1.1.3-rc.1")
+  implementation("io.arrow-kt:arrow-core:1.1.3")
+  implementation("io.arrow-kt:arrow-optics:1.1.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-  implementation("io.arrow-kt:arrow-fx-coroutines:1.1.3-rc.1")
+  implementation("io.arrow-kt:arrow-fx-coroutines:1.1.3")
 
   testImplementation("io.kotest:kotest-property:5.4.2")
   testImplementation("io.kotest:kotest-assertions-core:5.4.2")
