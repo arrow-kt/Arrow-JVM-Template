f5c53a0b765cd02357d9926a641a4df447ca181b
Imran Malic Settuba <46971368+i-walker@users.noreply.github.com>
2022-11-20 18:47:29 +0100

    update kotest arrow

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ dependencies {
 
   testImplementation("io.kotest:kotest-property:5.5.4")
   testImplementation("io.kotest:kotest-assertions-core:5.5.4")
-  testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
-  testImplementation("io.kotest.extensions:kotest-property-arrow:1.2.5") // optional
-  testImplementation("io.kotest.extensions:kotest-property-arrow-optics:1.2.5") // optional
+  testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.3.0")
+  testImplementation("io.kotest.extensions:kotest-property-arrow:1.3.0") // optional
+  testImplementation("io.kotest.extensions:kotest-property-arrow-optics:1.3.0") // optional
   testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
 }
